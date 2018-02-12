c2b6d7a488a8b2aa27b540bdb64cc595de4ba07a
Paul Harnack <harna100@mail.chapman.edu>
2018-02-11 17:33:30 -0800

    4-2 Using conditional statements
    Adds basic example of using an if/else statement

---
+++ Conditionals/Conditionals/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Conditionals
+//
+//  Created by Harnack, Paul (Student) on 2/11/18.
+//  Copyright © 2018 Harnack, Paul (Student). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let score:Int = 100
+        
+        if(score > 50000){
+            print("Player gets extra life")
+        }
+        else {
+            print("Not enough points for extra life")
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
