14f18d7eacd2f2964ca2a651a890431ff0354605
Paul Harnack <harna100@mail.chapman.edu>
2018-02-14 09:31:56 -0800

    Implements creation with core data

--- CoreDataTest/CoreDataTest/ViewController.swift
+++ CoreDataTest/CoreDataTest/ViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        createRecords()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +23,45 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func createRecords(){
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let entity = NSEntityDescription.entity(forEntityName: "Users", in: context)
+        
+        let newUser = NSManagedObject(entity: entity!, insertInto: context)
+        
+        newUser.setValue("Paul", forKey:"username")
+        newUser.setValue("1234", forKey:"password")
+        newUser.setValue("22", forKey:"age")
+        
+        do {
+            try context.save()
+        }
+        catch{
+            print("Failed to save")
+        }
+        
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
