e94aeff4d1e7ca8defa9cc0003a1c854e220862e
Paul Harnack <harna100@mail.chapman.edu>
2018-02-15 20:23:47 -0800

    Was attempting something with a treeview, don't remember what anymore

---
+++ TreeViewTest/Pods/Target Support Files/Pods-TreeViewTest/Pods-TreeViewTest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TreeViewTest : NSObject
+@end
+@implementation PodsDummy_Pods_TreeViewTest
+@end
---
+++ TreeViewTest/TreeViewTest/CustomNode.swift
@@ -0,0 +1,17 @@
+//
+//  CustomNode.swift
+//  TreeViewTest
+//
+//  Created by Harnack, Paul (Student) on 2/14/18.
+//  Copyright © 2018 Harnack, Paul (Student). All rights reserved.
+//
+
+import Foundation
+import LNZTreeView
+
+class CustomNode:TreeNodeProtocol {
+    var identifier: String = "Custom node"
+    
+    var isExpandable: Bool = true
+    
+}
