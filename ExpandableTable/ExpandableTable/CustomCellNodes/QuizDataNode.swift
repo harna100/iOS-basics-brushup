9566f462fda0cf57e209714cdf478443e7e1348b
Paul Harnack <harna100@mail.chapman.edu>
2018-02-16 18:42:33 -0800

    Initial creation of Quiz cells

---
+++ ExpandableTable/ExpandableTable/CustomCellNodes/QuizDataNode.swift
@@ -0,0 +1,9 @@
+//
+//  QuizDataNode.swift
+//  ExpandableTable
+//
+//  Created by Harnack, Paul (Student) on 2/16/18.
+//  Copyright © 2018 Harnack, Paul (Student). All rights reserved.
+//
+
+import Foundation
---
+++ ExpandableTable/ExpandableTable/CustomCells/QuizTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  QuizTableViewCell.swift
+//  ExpandableTable
+//
+//  Created by Harnack, Paul (Student) on 2/16/18.
+//  Copyright © 2018 Harnack, Paul (Student). All rights reserved.
+//
+
+import UIKit
+
+class QuizTableViewCell: CellType {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
